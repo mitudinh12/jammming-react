bde370e5d3ed60d255150caf105a3399fb456390
tu.dinh <tu.dinh@metropolia.fi>
2022-02-18 00:00:05 +0200

    Pass down info from App to Playlist to Tracklist to Track

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,7 +5,12 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-
+const playlistName = 'Chill';
+const playlistTracks = [
+  {name: 'Tiny Dancer', artist: 'Elton John', album: 'Madman Across The Water', id: '1'},
+  {name: 'Tiny Dancer', artist: 'Tim McGraw' , album: 'Love story', id: '2'},
+  {name: 'Tiny Dancer', artist: 'Rockabye Baby!', album: 'Lullaby Renditions of Elton John', id: '3'}
+]
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -22,7 +27,7 @@ class App extends React.Component {
             <SearchBar/>
           <div className="App-playlist">
             <SearchResults resultList={this.state.searchResults}/>
-            <Playlist/>
+            <Playlist playlistName={playlistName} playlistTracks={playlistTracks}/>
           </div>
         </div>
       </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -15,7 +15,7 @@ class Track extends React.Component {
             <div className="Track">
                 <div className="Track-information">
                     <h3>{this.props.track.name}</h3>
-                    <p>{this.props.track.artist+ ' | ' + this.props.track.albumn}</p>
+                    <p>{this.props.track.artist+ ' | ' + this.props.track.album}</p>
                 </div>
                 <button className="Track-action">+ or - will go here</button>
             </div>
